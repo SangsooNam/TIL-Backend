664d27adbae26e4f58ad3b24dc714e3b40328a63
CodeDiary18 <codediary18@gmail.com>
2024-07-10 04:31:46 +0900

    refactor: 토큰 발급/재발급 과정에 redis 로직 추가
    - 토큰 발급시 redis에 리프레시 토큰 저장
    - 재발급시 redis에서 리프레시 토큰 가져와 유효성 검증

--- til-domain/src/main/java/com/til/application/auth/AuthService.java
+++ til-domain/src/main/java/com/til/application/auth/AuthService.java
@@ -1,15 +1,19 @@
 package com.til.application.auth;
 
 import java.util.Map;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.til.config.db.RedisManager;
 import com.til.domain.auth.dto.AuthTokenDto;
 import com.til.domain.auth.dto.AuthUserInfoDto;
+import com.til.domain.auth.enums.AuthErrorCode;
 import com.til.domain.auth.enums.TokenType;
+import com.til.domain.auth.exception.TokenInvalidException;
 import com.til.domain.auth.provider.TokenProvider;
 
 import io.jsonwebtoken.Claims;
@@ -20,19 +24,21 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class AuthService {
 	private final TokenProvider tokenProvider;
+	private final RedisManager redisManager;
 
 	@Value("${jwt.access.expiration}")
 	private Long ACCESS_EXPIRE_DURATION;
 
 	@Value("${jwt.refresh.expiration}")
 	private Long REFRESH_EXPIRE_DURATION;
+
 	private static final String BEARER_TYPE = "Bearer";
 
 	public AuthTokenDto createToken(AuthUserInfoDto authUserInfoDto) {
 		String accessToken = generateToken(authUserInfoDto, TokenType.ACCESS);
 		String refreshToken = generateToken(authUserInfoDto, TokenType.REFRESH);
-
-		// TODO : Redis에 refresh token 저장
+		redisManager.setData(TokenType.REFRESH.name() + "_TOKEN:" + authUserInfoDto.email(), refreshToken,
+			REFRESH_EXPIRE_DURATION);
 
 		return AuthTokenDto.of(accessToken, refreshToken);
 	}
@@ -41,12 +47,19 @@ public class AuthService {
 		String refreshToken = resolveToken(bearerToken);
 		tokenProvider.validateToken(refreshToken);
 
-		// TODO : Redis에 저장된 refresh token과 비교
-
 		Claims claims = tokenProvider.parseClaims(refreshToken);
 		AuthUserInfoDto authUserInfoDto = AuthUserInfoDto.of(claims);
 
+		validateRefreshToken(authUserInfoDto.email(), refreshToken);
+
 		return createToken(authUserInfoDto);
+	}
+
+	public void validateRefreshToken(String key, String refreshToken) {
+		String redisToken = redisManager.getData(TokenType.REFRESH.name() + "_TOKEN:" + key);
+		if (!Objects.equals(refreshToken, redisToken)) {
+			throw new TokenInvalidException(AuthErrorCode.INVALID_TOKEN);
+		}
 	}
 
 	private String generateToken(AuthUserInfoDto authUserInfoDto, TokenType tokenType) {
@@ -67,4 +80,5 @@ public class AuthService {
 	private Long getExpireDuration(TokenType tokenType) {
 		return tokenType == TokenType.ACCESS ? ACCESS_EXPIRE_DURATION : REFRESH_EXPIRE_DURATION;
 	}
+
 }
