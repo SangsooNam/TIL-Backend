192bc81cbad38763c1f5cc8f6e7ea39f82472c22
DongYeon Kang <myjjue00@gmail.com>
2024-07-12 19:07:07 +0900

    refactor: allowedMethod(PATCH) 추가 및 allowedOrigin 경로 수정

--- til-api/src/main/java/com/til/config/WebConfig.java
+++ til-api/src/main/java/com/til/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.til.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+	@Value("${cors.allowed.origin}")
+	private String ALLOWED_ORIGIN_URL;
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -14,8 +17,8 @@ public class WebConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:3000")
-					.allowedMethods("GET", "POST", "PUT", "DELETE")
+					.allowedOrigins(ALLOWED_ORIGIN_URL)
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 					.allowedHeaders("*")
 					.allowCredentials(true);
 			}
